b77cfdc5d497250f19ccd95859dc020b9ac408ad
Shweta-akki <shwetaakki2002@gmail.com>
2026-07-18 23:10:30 +0530

    added task to Day 1

---
+++ Day 1/Band Name Generator Project/task5.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+City = input("What city did you grow up?\n")
+pet = input("What pet did you grow up?\n")
+print(" Your band name is " + City + " " + pet)
